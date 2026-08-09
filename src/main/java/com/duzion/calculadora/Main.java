575d57df56567d21b2d40c590e3be532c7a4cd85
CarlosEduardoAlves <edualves.tech@gmail.com>
2026-08-09 11:39:14 -0300

    Alterando a feature

--- src/main/java/com/duzion/calculadora/Main.java
+++ src/main/java/com/duzion/calculadora/Main.java
@@ -7,7 +7,7 @@ import com.duzion.calculadora.OperacaoFactory;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("\"Versão Experimental\"");
+        System.out.println("Texto da FEATURE");
         EntradaDados entrada = new EntradaDados();
 
         // Lê os números e a operação
